bbbe46168f9f4e30062981753e7b715c8ddbb08a
dkxce <25371256+dkxce@users.noreply.github.com>
2023-06-27 14:37:53 +0300

    Add files via upload

--- IniSaved.cs
+++ IniSaved.cs
@@ -1,11 +1,12 @@
 ﻿//
 // C# (cross-platform)
 // IniSaved
-// v 0.1, 26.06.2023
+// v 0.2, 27.06.2023
 // https://github.com/dkxce/INISaved
 // en,ru,1251,utf-8
 //
 
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
@@ -22,6 +23,7 @@ namespace System.Xml
     public class IniSaved<T>
     {
         public static bool presaveFileHeader = true;
+        public static bool presaveXmlSerialization = true;
         public static string customHeaderLine = null;
 
         #region SAVE
@@ -61,7 +63,8 @@ namespace System.Xml
             if (string.IsNullOrEmpty(section))
             {
                 section = typeof(T).Name;
-                IniNameAttribute ina = typeof(T).GetCustomAttribute<IniNameAttribute>();
+                if (type.IsArray) section = $"ArrayOf{section.Replace("[]", "")}";
+                IniSectionAttribute ina = typeof(T).GetCustomAttribute<IniSectionAttribute>();
                 if (ina != null && !String.IsNullOrEmpty(ina.name)) section = ina.name;
             };
 
@@ -77,16 +80,26 @@ namespace System.Xml
             writer.Close();
             string xml = Encoding.UTF8.GetString(bb);
 
+            // TEST //
+            /*
+            XmlSerializer xxs = new XmlSerializer(typeof(T));
+            StreamReader reader = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(xml)));
+            T c = (T)xxs.Deserialize(reader);
+            reader.Close();
+            */
+
             // XML -> INI
             XmlDocument xd = new XmlDocument();
             xd.LoadXml(xml);
             Dictionary<string, int> sections = new Dictionary<string, int>();
             string res = GetIni(xd.DocumentElement, section, sections);
+            if (presaveXmlSerialization)
+                res = ";" + xml.Replace("\r\n", "\r\n;") + "\r\n;\r\n\r\n" + res;
             return res;
         }
 
         private static string GetIni(XmlNode xn, string section, Dictionary<string, int> sections)
-        {                        
+        {
             bool hasRoot = false;
             bool hasBody = false;
 
@@ -99,7 +112,7 @@ namespace System.Xml
             if (xn.Attributes != null)
                 foreach (XmlAttribute a in xn.Attributes)
                 {
-                    AddRoot(ref hasRoot, sb, section);                    
+                    AddRoot(ref hasRoot, sb, section);
                     sb.Append($"@{NormalizeValue(a.Name, true)}={NormalizeValue(a.Value)}\r\n");
                     hasBody = true;
                 };
@@ -107,29 +120,29 @@ namespace System.Xml
             List<XmlNode> simpleNodes = new List<XmlNode>();
             List<XmlNode> detailNodes = new List<XmlNode>();
 
-            if(xn.HasChildNodes)
+            if (xn.HasChildNodes)
                 foreach (XmlNode n in xn.ChildNodes)
                 {
                     if (n.NodeType == XmlNodeType.Text)
                     {
-                        AddRoot(ref hasRoot, sb, section);                        
+                        AddRoot(ref hasRoot, sb, section);
                         sb.Append($"@={NormalizeValue(n.Value)}\r\n");
                         hasBody = true;
                     }
                     else if (n.HasChildNodes && n.ChildNodes.Count == 1 && n.ChildNodes[0].NodeType == XmlNodeType.Text && (n.Attributes == null || n.Attributes.Count == 0))
                         simpleNodes.Add(n);
                     else
-                        detailNodes.Add(n);                    
+                        detailNodes.Add(n);
                 };
 
-            foreach(XmlNode n in simpleNodes)
+            foreach (XmlNode n in simpleNodes)
             {
-                AddRoot(ref hasRoot, sb, section);                
+                AddRoot(ref hasRoot, sb, section);
                 sb.Append($"{NormalizeValue(n.Name, true)}={NormalizeValue(n.ChildNodes[0].Value)}\r\n");
                 hasBody = true;
             };
 
-            if(simpleNodes.Count > 0) sb.Append("\r\n");
+            if (simpleNodes.Count > 0) sb.Append("\r\n");
 
             foreach (XmlNode n in detailNodes)
             {
@@ -139,7 +152,7 @@ namespace System.Xml
                 hasBody = true;
             };
 
-            if(hasBody) sb.Append("\r\n");
+            if (hasBody) sb.Append("\r\n");
             return sb.ToString();
         }
 
@@ -154,7 +167,8 @@ namespace System.Xml
 
         #endregion SAVE
 
-        #region Load
+        #region LOAD
+
         public static void Load(StreamReader sr, ref T obj, string section = null)
         {
             object v = obj;
@@ -204,7 +218,6 @@ namespace System.Xml
         public static void Load(StreamReader sr, Type type, ref object obj, string section = null)
         {
             string xmlbase = "";
-            if(!string.IsNullOrEmpty(section))
             {
                 XmlSerializerNamespaces ns = new XmlSerializerNamespaces(); ns.Add("", "");
                 XmlSerializer xxs = new XmlSerializer(typeof(T));
@@ -224,22 +237,24 @@ namespace System.Xml
             if (string.IsNullOrEmpty(section))
             {
                 section = typeof(T).Name;
-                IniNameAttribute ina = typeof(T).GetCustomAttribute<IniNameAttribute>();
+                if (type.IsArray) section = $"ArrayOf{section.Replace("[]", "")}";
+                IniSectionAttribute ina = typeof(T).GetCustomAttribute<IniSectionAttribute>();
                 if (ina != null && !String.IsNullOrEmpty(ina.name)) section = ina.name;
             };
 
             // READ INI SECTIONS
             List<KeyValuePair<string, List<string>>> secSorted = null;
-            {                
+            {
                 string currsect = "";
                 Dictionary<string, List<string>> sections = new Dictionary<string, List<string>>();
                 while (!sr.EndOfStream)
                 {
                     string ln = sr.ReadLine().Trim();
                     if (string.IsNullOrEmpty(ln) || ln.StartsWith("#") || ln.StartsWith(";")) continue;
-                    if (ln.StartsWith("[")) { 
-                        currsect = ReversizeValue(ln.Split('#', ';')[0].Trim('[', ']')); 
-                        sections.Add(currsect, new List<string>()); continue; 
+                    if (ln.StartsWith("["))
+                    {
+                        currsect = ReversizeValue(ln.Split('#', ';')[0].Trim('[', ']'));
+                        sections.Add(currsect, new List<string>()); continue;
                     };
                     if (string.IsNullOrEmpty(currsect)) continue;
                     if (ln.IndexOf("=") < 0) continue;
@@ -248,9 +263,9 @@ namespace System.Xml
                 };
                 // SORT
                 secSorted = new List<KeyValuePair<string, List<string>>>(sections);
-                secSorted.Sort((KeyValuePair<string, List<string>> a, KeyValuePair<string, List<string>> b) => a.Key.CompareTo(b.Key));
+                //secSorted.Sort(new CustomComparer());
                 // REPLACE ROOT
-                if(secSorted.Count > 0 && !string.IsNullOrEmpty(section) && !string.IsNullOrEmpty(xmlbase))
+                if (secSorted.Count > 0 && !string.IsNullOrEmpty(section) && !string.IsNullOrEmpty(xmlbase))
                 {
                     List<KeyValuePair<string, List<string>>> tmps = new List<KeyValuePair<string, List<string>>>();
                     foreach (KeyValuePair<string, List<string>> kvp in secSorted)
@@ -271,7 +286,7 @@ namespace System.Xml
             XmlDeclaration xml_declaration = xd.CreateXmlDeclaration("1.0", "utf-8", null);
             xd.InsertBefore(xml_declaration, xd.DocumentElement);
 
-            foreach(KeyValuePair<string, List<string>> s in secSorted)
+            foreach (KeyValuePair<string, List<string>> s in secSorted)
             {
                 string fullKey = s.Key;
                 string shrtKey = s.Key;
@@ -279,19 +294,31 @@ namespace System.Xml
                 // Normalize Key (strip .\d$)
                 {
                     int iof = fullKey.LastIndexOf(".");
-                    if (iof > 0 && int.TryParse(fullKey.Substring(iof + 1), out _)) 
+                    if (iof > 0 && int.TryParse(fullKey.Substring(iof + 1), out _))
                         fullKey = fullKey.Substring(0, iof);
                 };
 
                 // Find/Create Path
                 XmlNode thisRoot = xd;
                 if (fullKey.Contains("."))
-                {                    
+                {
                     string[] paths = fullKey.Split('.');
                     for (int i = 0; i < paths.Length - 1; i++)
                     {
-                        XmlNode sn = thisRoot.SelectSingleNode(paths[i]+"[last()]");
-                        if (sn == null) sn = thisRoot.AppendChild(xd.CreateElement(paths[i]));
+                        string cpath = NormalizeValue(paths[i]);
+                        int pos = -1;
+                        if (!int.TryParse(cpath, out pos)) pos = -1;
+                        XmlNode sn = null;
+                        if (pos < 0)
+                            sn = thisRoot.SelectSingleNode(cpath + "[last()]");
+                        else
+                        {
+                            cpath = thisRoot.Name;
+                            thisRoot = thisRoot.ParentNode;
+                            sn = thisRoot.SelectSingleNode(cpath + $"[{pos + 1}]");
+                        };
+                        if (sn == null)
+                            sn = thisRoot.AppendChild(xd.CreateElement(cpath));
                         thisRoot = sn;
                     };
                     shrtKey = paths[paths.Length - 1];
@@ -300,9 +327,9 @@ namespace System.Xml
                 // Create Node
                 XmlNode xn = xd.CreateNode(XmlNodeType.Element, null, shrtKey, null);
                 thisRoot = thisRoot.AppendChild(xn);
-                
+
                 // Fill Node
-                List<KeyValuePair<string,string>> attrs = new List<KeyValuePair<string, string>>();
+                List<KeyValuePair<string, string>> attrs = new List<KeyValuePair<string, string>>();
                 List<KeyValuePair<string, string>> vars = new List<KeyValuePair<string, string>>();
                 foreach (string line in s.Value)
                 {
@@ -317,7 +344,16 @@ namespace System.Xml
                     }
                     else if (name.StartsWith("@"))
                     {
-                        (xn as XmlElement).SetAttribute(name.Substring(1), value);
+                        string attr = name.Substring(1);
+                        if (!attr.StartsWith("xmlns:") && attr.Contains(":"))
+                        {
+                            string[] attrdel = attr.Split(':');
+                            XmlAttribute a = xd.CreateAttribute(attrdel[0], attrdel[1], "http://www.w3.org/2001/XMLSchema-instance");
+                            a.Value = value;
+                            (xn as XmlElement).SetAttributeNode(a);
+                        }
+                        else
+                            (xn as XmlElement).SetAttribute(attr, value);
                     }
                     else
                     {
@@ -328,7 +364,7 @@ namespace System.Xml
                     };
                 };
             };
-            
+
             string res = xd.OuterXml;
 
             XmlSerializer xs = new XmlSerializer(typeof(T));
@@ -338,7 +374,38 @@ namespace System.Xml
             obj = c;
         }
 
+        public class CustomComparer : IComparer<KeyValuePair<string, List<string>>>
+        {
+            public int Compare(KeyValuePair<string, List<string>> x, KeyValuePair<string, List<string>> y)
+            {
+                string a = x.Key;
+                string b = y.Key;
+                string s = FindSame(a, b);
+
+                if (!string.IsNullOrEmpty(s))
+                {
+                    string av = a.Substring(s.Length); if (av.Length > 0) av = av.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries)[0];
+                    string bv = b.Substring(s.Length); if (bv.Length > 0) bv = bv.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries)[0];
+                    if (int.TryParse(av, out int ai) && int.TryParse(bv, out int bi)) return ai.CompareTo(bi);
+                    if (int.TryParse(av, out _)) return 1;
+                    if (int.TryParse(bv, out _)) return -1;
+                };
+                return a.CompareTo(b);
+            }
+
+            private string FindSame(string a, string b)
+            {
+                if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b)) return null;
+                string r = "";
+                for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+                    if (a[i] == b[i]) r += a[i]; else break;
+                return r;
+            }
+        }
+
         #endregion LOAD         
+
+        #region STATIC
 
         public static string CurrentDirectory()
         {
@@ -377,17 +444,52 @@ namespace System.Xml
         public static string ReversizeValue(string value, bool name = false)
         {
             if (name) value = value.Replace("\\u0040", "@");
-            value = value.Replace("\\u003B", ";");
+            value = value.Replace("\\u003B", ";").Replace("\\u003b", ";");
             value = value.Replace("\\u0023", "#");
-            value = value.Replace("\\u000D", "\r");
-            value = value.Replace("\\u000A", "\n");
+            value = value.Replace("\\u000D", "\r").Replace("\\u000d", "\r");
+            value = value.Replace("\\u000A", "\n").Replace("\\u000a", "\n");
             return value;
         }
-    }    
 
-    public class IniNameAttribute : System.Attribute
+        #endregion STATIC
+    }
+
+    public class IniSectionAttribute : Attribute
     {
         public string name { set; get; }
-        public IniNameAttribute(string name) { this.name = name; }
-    }    
+        public IniSectionAttribute(string name) { this.name = name; }
+    }
+
+    /// <summary>
+    ///     Class for Serialize Dictionary
+    /// </summary>
+    public class DictionaryEntry
+    {
+        public object Key;
+        public object Value;
+
+        public DictionaryEntry() { }
+        public DictionaryEntry(object key, object value) { Key = key; Value = value; }
+
+        public static DictionaryEntry[] Get(IDictionary dict)
+        {
+            List<DictionaryEntry> entries = new List<DictionaryEntry>(dict == null ? 0 : dict.Count);
+            if (dict != null)
+                foreach (object key in dict.Keys)
+                    entries.Add(new DictionaryEntry(key, dict[key]));
+            return entries.ToArray();
+        }
+
+        public static void Set(IDictionary dict, List<DictionaryEntry> values)
+        {
+            Set(dict, values?.ToArray());
+        }
+
+        public static void Set(IDictionary dict, DictionaryEntry[] values)
+        {
+            dict.Clear();
+            if (values == null || values.Length == 0) return;
+            foreach (DictionaryEntry entry in values) dict.Add(entry.Key, entry.Value);
+        }        
+    }
 }
---
+++ IniSavedSample.cs
@@ -0,0 +1,189 @@
+﻿//
+// C# (cross-platform)
+// IniSaved
+// v 0.2, 27.06.2023
+// https://github.com/dkxce/INISaved
+// en,ru,1251,utf-8
+//
+
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace System.Xml.IniSavedSample
+{
+    [IniSection("DEFAULT")] // OPTIONAL
+    public class SampleConfig: IniSaved<SampleConfig>
+    {
+        #region Simple
+        public string Name = "SampleConfiguration";
+        public int Age = 18;
+        [XmlElement("BirthDay")]
+        public DateTime Birth = DateTime.Today;
+        public float Weight = 101f;
+        public long Length = 777;
+        public bool Man = true;
+        #endregion Simple
+
+        #region Arrays
+        public byte[] aBytes = new byte[] { 0x01, 0x10, 0xFF, 0x0A, 0x0D, 0xCD, 0x3B, 0x23 };
+        public double[] aDoubles = new double[] { 11.0, 22.0, 33.0, 44.0, 55.0, 66.0, 77.0 };
+        public int[] aInts = new int[] { 101, 202, 303, 404, 505, 606, 707 };
+        public SubSampleConfig[] aSubSampleConfigs = new SubSampleConfig[] {
+            new SubSampleConfig() { comment = "com", key = "kname", description = "vval" },
+            new SubSampleConfig() { comment = "fav", key = "movek", description = "loop" },
+            new SubSampleConfig() { comment = "moc", key = "emank", description = "lavv" }
+        };
+        #endregion Arrays
+
+        #region Lists
+        public List<string> lStrings = new List<string>() { "s1", "s2", "s3", "s4" };
+        public List<int> lInts = new List<int>() { byte.MaxValue, short.MaxValue, ushort.MaxValue };
+        public List<SubSampleRecord> lSubSampleRecords = new List<SubSampleRecord>() { new SubSampleRecord() { person = "Unknown", mail = "noMail", web = "http://" } };
+        #endregion Lists
+
+        #region Dictionaries
+
+        [XmlIgnore]
+        public Dictionary<string, int> IntDict = new Dictionary<string, int>() { { "IntDict_A", -101 }, { "IntDict_B", -2022 } };
+
+        [XmlArray("IntDict")]
+        public DictionaryEntry[] IntDictAsList
+        {
+            get { return DictionaryEntry.Get(IntDict); }
+            set { DictionaryEntry.Set(IntDict = new Dictionary<string, int>(), value); }
+        }
+
+        [XmlIgnore]
+        public Dictionary<string, string> StringDict = new Dictionary<string, string>() { { "StringDict_C", "s1088" }, { "StringDict_D", "s2088" } };
+
+        [XmlArray("StringDict")]
+        public DictionaryEntry[] StringDictAsList
+        {
+            get { return DictionaryEntry.Get(StringDict); }
+            set { DictionaryEntry.Set(StringDict = new Dictionary<string, string>(), value); }
+        }
+
+        #endregion Dictionaries      
+    }
+
+    public class SubSampleRecord
+    {
+        [XmlAttribute]
+        public string person;
+
+        [XmlAttribute]
+        public string mail;
+
+        [XmlAttribute]
+        public string web;
+    }
+
+    public class SubSampleConfig
+    {
+        [XmlAttribute]
+        public string key;
+
+        [XmlAttribute]
+        public string comment;
+
+        [XmlText]
+        public string description;
+
+        [XmlIgnore]
+        public Dictionary<string, float> FloatDict = new Dictionary<string, float>() { { "FloatDictSictA", 11f }, { "FloatDictSictB", 22f }, { "FloatDictSictC", 33f } };
+
+        [XmlArray("FloatDict")]
+        public DictionaryEntry[] FloatDictAsList
+        {
+            get { return DictionaryEntry.Get(FloatDict); }
+            set { DictionaryEntry.Set(FloatDict = new Dictionary<string, float>(), value); }
+        }
+    }
+}
+
+namespace System.Xml.IniSavedSampleBlank
+{
+    [IniSection("DEFAULT")] // OPTIONAL
+    public class SampleConfig : IniSaved<SampleConfig>
+    {
+        #region Simple
+        public string Name;
+        public int Age;
+        public DateTime Birth;
+        public float Weight;
+        public long Length;
+        public bool Man;
+        #endregion Simple
+
+        #region Arrays
+        public byte[] aBytes;
+        public double[] aDoubles;
+        public int[] aInts;
+        public SubSampleConfig[] aSubSampleConfigs;
+        #endregion Arrays
+
+        #region Lists
+        public List<string> lStrings = new List<string>();
+        public List<int> lInts = new List<int>();
+        public List<SubSampleRecord> lSubSampleRecords = new List<SubSampleRecord>();
+        #endregion Lists
+
+        #region Dictionaries
+
+        [XmlIgnore]
+        public Dictionary<string, int> IntDict = new Dictionary<string, int>();
+
+        [XmlArray("IntDict")]
+        public DictionaryEntry[] IntDictAsList
+        {
+            get { return DictionaryEntry.Get(IntDict); }
+            set { DictionaryEntry.Set(IntDict = new Dictionary<string, int>(), value); }
+        }
+
+        [XmlIgnore]
+        public Dictionary<string, string> StringDict = new Dictionary<string, string>();
+
+        [XmlArray("StringDict")]
+        public DictionaryEntry[] StringDictAsList
+        {
+            get { return DictionaryEntry.Get(StringDict); }
+            set { DictionaryEntry.Set(StringDict = new Dictionary<string, string>(), value); }
+        }
+
+        #endregion Dictionaries      
+    }
+
+    public class SubSampleRecord
+    {
+        [XmlAttribute]
+        public string person;
+
+        [XmlAttribute]
+        public string mail;
+
+        [XmlAttribute]
+        public string web;
+    }
+
+    public class SubSampleConfig
+    {
+        [XmlAttribute]
+        public string key;
+
+        [XmlAttribute]
+        public string comment;
+
+        [XmlText]
+        public string description;
+
+        [XmlIgnore]
+        public Dictionary<string, float> FloatDict = new Dictionary<string, float>();
+
+        [XmlArray("FloatDict")]
+        public DictionaryEntry[] FloatDictAsList
+        {
+            get { return DictionaryEntry.Get(FloatDict); }
+            set { DictionaryEntry.Set(FloatDict = new Dictionary<string, float>(), value); }
+        }
+    }
+}
