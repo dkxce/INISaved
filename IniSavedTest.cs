10083191aab04e88fb038da21db48ff0daa6e4e8
dkxce <25371256+dkxce@users.noreply.github.com>
2023-06-27 14:48:17 +0300

    Add files via upload

---
+++ IniSavedTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Write
+            {
+                IniSaved<System.Xml.IniSavedSample.SampleConfig>.presaveFileHeader = true;
+                IniSaved<System.Xml.IniSavedSample.SampleConfig>.presaveXmlSerialization = false;
+                IniSaved<System.Xml.IniSavedSample.SampleConfig>.customHeaderLine = "Original at https://github.com/dkxce/INISaved";
+                System.Xml.IniSavedSample.SampleConfig CONFIG = new System.Xml.IniSavedSample.SampleConfig();
+                string iniData = IniSaved<System.Xml.IniSavedSample.SampleConfig>.Save(CONFIG);
+                IniSaved<System.Xml.IniSavedSample.SampleConfig>.Save("sample.ini", CONFIG);
+                Console.WriteLine(iniData);
+            };
+
+            // Read CONFIG
+            {
+                System.Xml.IniSavedSampleBlank.SampleConfig CONFIG = new System.Xml.IniSavedSampleBlank.SampleConfig();
+                CONFIG = IniSaved<System.Xml.IniSavedSampleBlank.SampleConfig>.Load("sample.ini");
+            };
+        }
+    }
+}
